090ef401830e834b3aa177abbd9d0cea5ca5efe9
hyemijeong <gpal6072@naver.com>
2021-11-29 22:04:34 +0900

    아이콘 연결

--- App/code/RECEIPT_del_edit/app/src/main/java/com/example/receipt/Data.kt
+++
@@ -1,8 +0,0 @@
-package com.example.receipt
-
-import android.graphics.Bitmap
-import java.io.Serializable
-
-data class Data (
-    val bit : Bitmap
-): Serializable
--- App/code/RECEIPT_del_edit/app/src/main/java/com/example/receipt/Profiles.kt
+++
@@ -1,7 +0,0 @@
-package com.example.receipt
-
-import java.time.LocalDate
-
-class Profiles(val item: Int, val name: String, val categories: String, val dates: LocalDate) {
-
-}
